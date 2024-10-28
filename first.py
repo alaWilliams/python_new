b85a17006005a11deaa858aa27e7c6dbdc57568f
Ala Williams <alicja.m.williams@gmail.com>
2024-10-28 10:30:22 +0200

    first commit

---
+++ first.py
@@ -0,0 +1,7 @@
+print("Hello Python!")
+
+length = int(input('Enter the length: '))
+width = int(input('Enter the width: '))
+area = length * width
+
+print(f'Length: {length} and width: {width}. So area of a rectangle is {area}')
